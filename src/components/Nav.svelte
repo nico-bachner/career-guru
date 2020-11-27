9b253fb0c077eb0f2c2bd80438fb827d46da134f
Nico Bachner <yo@nicob.dev>
2020-11-27 19:08:36 +0100

    removed default blog

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,11 +7,9 @@
 		background-color: lightblue;
 		padding: .5rem;
 	}
-
 	[aria-current] {
 		color: chocolate;
 	}
-
 	a {
 		margin: 1em;
 	}
@@ -20,8 +18,4 @@
 <nav>
 	<a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a>
 	<a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-	<a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a>
 </nav>
