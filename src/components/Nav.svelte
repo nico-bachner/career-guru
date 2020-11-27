ba0c4acbaf376246b43c27a2c851599261d06d44
Nico Bachner <yo@nicob.dev>
2020-11-27 18:36:32 +0100

    initial project setup

---
+++ src/components/Nav.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+	nav {
+		background-color: lightblue;
+		padding: .5rem;
+	}
+
+	[aria-current] {
+		color: chocolate;
+	}
+
+	a {
+		margin: 1em;
+	}
+</style>
+
+<nav>
+	<a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a>
+	<a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a>
+
+		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		     the blog data when we hover over the link or tap it on a touchscreen -->
+	<a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a>
+</nav>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<style>
+	h1 {
+		text-align: center;
+		font-size: 4em;
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Welcome to CareerGuru!</h1>
