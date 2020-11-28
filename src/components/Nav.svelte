be788254e3eb28aba13355320e9b145cf88874fa
cjlaserna <clasernaj@gmail.com>
2020-11-27 19:26:21 -0800

    fixed nav, abt, and global css"

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,25 +3,69 @@
 </script>
 
 <style>
-	nav {
-		width: fit-content;
-		margin: 2em auto;
+	/* navbar */
+	nav{
+		background-color: white;
+		display: block;
+		
+		width: 100%	;
+		height: 4vw;
 	}
-	[aria-current] {
-		color: chocolate;
+	.logo{
+		margin: 10px;
+		width: 8vw;
+		
+		position: absolute;
+		left:0;
 	}
-	button {
-		display: inline-block;
-		padding: .5em;
-		text-transform: uppercase;
+	
+	.navbar ul{
+		list-style-type:none;
+		overflow:hidden;
+		
+		position: absolute;
+		right:0;
+		top: 1vw;
 	}
+	.navbar li{
+		float: left;
+		
+		margin-right: 1vw;
+		padding: .5vw 1vw;
+	}
+	.navbar li a{
+		display:inline-block;
+		color: #555B6E;
+		
+		font-family: 'Work Sans', sans-serif;
+		font-weight: 500;
+		text-transform: capitalize;
+		font-size: 1.25vw;
+		text-decoration:none;
+		text-align:center;
+		vertical-align:middle;
+		
+	}
+
+	.navbar li:hover{
+		background-color: rgba(190, 227, 219, 0.2);  
+		border-radius: 10px;
+	}
+	li[aria-current] {
+		background-color: rgba(190, 227, 219, 0.2);  
+		border-radius: 10px;	
+	}
+	/* end of navbar */
 </style>
 
-<nav>
-	<a aria-current="{segment === undefined ? 'page' : undefined}" href="."><button>home</button></a>
-	<a aria-current="{segment === 'about' ? 'page' : undefined}" href="about"><button>about</button></a>
-	<a aria-current="{segment === 'forum' ? 'page' : undefined}" href="forum"><button>forum</button></a>
-	<a aria-current="{segment === 'chat' ? 'page' : undefined}" href="chat"><button>chat</button></a>
-	<a aria-current="{segment === 'login' ? 'page' : undefined}" href="login"><button>log in</button></a>
-	<a aria-current="{segment === 'signup' ? 'page' : undefined}" href="signup"><button>sign up</button></a>
+<nav class="navbar">
+	<img class="logo" src="logo.png" alt="logo">
+	<ul>
+		<li aria-current="{segment === undefined ? 'page' : undefined}"><a href=".">Home</a></li>
+		<li aria-current="{segment === 'about' ? 'page' : undefined}"> <a href="about">About</a></li>
+		<li aria-current="{segment === 'forum' ? 'page' : undefined}"><a href="forum">Forum</a></li>
+		<li aria-current="{segment === 'chat' ? 'page' : undefined}"><a href="chat">Chat</a></li>
+		<li aria-current="{segment === 'login' ? 'page' : undefined}"><a href="chat">Login</a></li>
+		<li aria-current="{segment === 'signup' ? 'page' : undefined}"><a href="chat">Signup</a></li>
+	</ul>
 </nav>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,112 @@
 <svelte:head>
-	<title>About</title>
+	<!-- Meta -->
+	<title> CareerGuru - About </title>
+	<meta charset="UTF-8">
+	<meta content="text/html; charset=utf-8" http-equiv="content-type">
+	
+	<!-- desc / author / keywords-->
+	<meta content="Chat with professionals and plan your career as a highschool student." name="description">
+	<meta content="CareerGuru, highschool, students, volunteer,, mentorship" name="keywords">
+	<!-- end -->
+	
+	<!-- compatibility -->
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="true">
+	<!-- end of compatibility -->
+	
+	<!-- End of Meta -->
+	
+	<!-- libraries -->
+
+	<!-- scripts -->
+	<!-- <script type="text/javascript" src="urlgohere"></script> -->
 </svelte:head>
 
-<h1>What is our mission?</h1>
+<style>
+	:root {
+		display: block;
+	}
 
-<p></p>
+	/* about block */
+	.about.block{
+		height: 50%;
+		width: 75%;
+	}
+	.abt-centerpiece{
+		width: 200%;
+	}
+	.abt-centerpiece img{
+		max-width:100%;
+		max-height:100%;
+	}
+	/* end of about block */
+	
+	/* testimblock */
+	.testim.block{
+		width: 75%;
+		flex-direction: column;
+	}
+	.testimgrid{
+		display:flex;
+		justify-content: center;
+		align-items:center;
+		flex-direction: row;
+	}
+	.griditem{
+		display:flex;
+		justify-content: center;
+		align-items:center;
+		flex-direction: column;
+		
+		width: 100%;
+		height:25vh;
+		background-color:#fff;
+		margin: 15px;
+	}
+	.griditem .itemcontent{
+		text-align:center;
+		background-color:#fff;
+	}
+	.griditem .itemauthor{
+		height: 10vh;
+		color: white;
+		background-color:#555B6E;
+	}
+	/* end of testim block */
+	
+</style>
+<div class="content">
+	<div class="about block">
+		<div class="abt-text">
+			<div class="heading">
+				<h1>About Us</h1>
+			</div>
+			<div class="description">
+				<p>CareerGuru connects students with professional mentors to learn more about their potential fields.</p>
+				<br>
+				<p>As the world shifts towards virtual oriented workspaces, students find themselves looking to make connections with professionals through sites like LinkedIn, Indeed, etc. This site makes the networking process for highschool students, all the more easier.</p>
+			</div>
+		</div>
+		<div class="abt-centerpiece"><img src="aboutimg.png" alt="People working together"></div>
+	</div>
+	<div class="testim block">
+		<div class="subheading">
+			<h2>Testimonials<h2>
+		</div>
+		<div class="testimgrid">
+			<div class="griditem">
+			<p class="griditem itemcontent">“I've always struggled with finding what I really want to do in life. Career Guru is a great opportunity to explore that.” </p>
+			<p class="griditem itemauthor">AnnMarie Vo, 16</p>
+			</div>
+			<div class="griditem">
+			<p class="griditem itemcontent">“Choosing a career is such a tedious process. I’m glad to have Career Guru on my side.”  </p>
+			<p class="griditem itemauthor">Anna Thomas, 16</p>
+			</div>
+			<div class="griditem">
+			<p class="griditem itemcontent">“I really like the idea of having a platform to explore different careers. Career Guru makes the future less intimidating.” </p>
+			<p class="griditem itemauthor">Angelina Vuong, 16</p>
+			</div>
+		</div>
+	</div>
+</div>
