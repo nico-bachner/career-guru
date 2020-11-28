0e2db0db131eab65c6ad1451d3d000da64c7d19c
electroqobra <electroqobra@gmail.com>
2020-11-28 11:43:56 -0500

    Removed bottom navigation

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -26,20 +26,6 @@
     }
 </style>
 
-<footer class="grid grid-cols-4 gap-5">
-    <img src="logo.png" alt="Career Guru logo">
-    <ul>
-        <li><a href="about">Home</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="about">Forum</a></li>
-    </ul>
-    <address>
-        Email: <a href="mailto:john.doe@example.com">mailto:john.doe@example.com</a>
-        <br>
-        Phone: <a href="tel:+10123456789">+10123456789</a>
-        <br>
-        Address: 2345 Mailing, CA, US
-    </address>
-
+<footer>
     <img class="decoration" src="dot-grid.svg" alt="Career Guru logo">
 </footer>
