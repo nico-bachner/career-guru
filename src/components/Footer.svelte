0f7cf45ffefb56e85e8f59fbe826c1dc56c3b895
cjlaserna <clasernaj@gmail.com>
2020-11-28 13:39:05 -0800

    changed footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,31 +1,77 @@
 <style>
-    img {
-		width: 100%;
+    /* footer styles */
+    footer{
+        width: 100vw;
+        position:relative;
+        height: 10vw;
+        background-color: white;
     }
-    @media screen and (min-width: 500px) {
-		footer {
-			display: grid;
-            grid-template-columns: 1fr 1fr;
-            gap: 2em;
-		}
-		.decoration {
-            grid-row: 3;
-            grid-column: span 2;
-        }
+    footer .container{overflow: hidden;}
+    #footer-grid{
+        background-color:white;
+        height: 4vw;
+        width: 100vw;
+        display:block;
+        background-size: 50%;
+        background-repeat:repeat-x;
+        margin: 0;
     }
-    @media screen and (min-width: 1000px) {
-		footer {
-			display: grid;
-            grid-template-columns: 1fr 1fr 1fr 1fr;
-            gap: 2em;
-		}
-		.decoration {
-            grid-row: 2;
-            grid-column: span 4;
-        }
+    .logo{
+        margin: 10px;
+        width: 6vw;
+        float:left;
     }
+    footer p{
+        font-family: Work Sans, Sans Serif;
+        font-weight: 500;
+        font-size: .75vw;
+        text-decoration:none;
+        color: #555B6E;
+    }
+    .quicklinks{
+        float:left;
+        margin-left:25px;
+        height:50%;
+        display:block;
+    }
+    .quicklinks ul{
+        list-style-type:none;
+    }
+    .quicklinks li a{
+        font-family: Work Sans, Sans Serif;
+        font-weight: 400;
+        font-size: .75vw;
+        text-decoration:none;
+        color: #555B6E;
+    }
+    .quicklinks li a:hover{
+        text-decoration:underline;
+    }
+    .contact{
+        height: 50%;
+        margin-left: 10vw;
+        margin-top: 1vw;
+    }
+    /* end of footer */
 </style>
 
 <footer>
-    <img class="decoration" src="dot-grid.svg" alt="Career Guru logo">
+    <div class="container">
+        <div class="quicklinks">
+            <img src="logo.png" alt="Career Guru logo" class="logo">
+            <ul>
+                <li><a href="about">About</a></li>
+                <li><a href="forum">Forum</a></li>
+            </ul>
+        </div>
+        <div class="contact">
+            <p>Contact information</p>
+            <address>
+                <p>Email: <a href="mailto:john.doe@example.com">john.doe@example.com</a></p>
+                <p>Phone: <a href="tel:+10123456789">+10123456789</a></p>
+                <p>Mailing Address: 2345 Mailing, CA, US</p>
+            </address>
+        </div>
+        <div id="footer-grid" style="background-image: url('dot-grid.svg')"></div>
+    </div>
 </footer>
