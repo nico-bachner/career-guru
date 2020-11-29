366e62bf50b36bfc83b9872ad56cfdde3d849aef
Nico Bachner <yo@nicob.dev>
2020-11-29 10:08:42 +0100

    added favicon

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,19 +5,19 @@
 
 <style>
 	main {
-		padding: 5em 2em;
+		padding: 0 2em;
 		min-height: 70vh;
 		background-color: var(--background);
 		color: var(--content);
 	}
 	@media screen and (min-width: 500px) {
 		main {
-			padding: 5em 4em;
+			padding: 3em 4em;
 		}
 	}
 	@media screen and (min-width: 1000px) {
 		main {
-			padding: 5em 6em;
+			padding: 6em;
 		}
 	}
 
