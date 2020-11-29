8cc40bbfb4209453f531968f2b8a67a4610b285e
saltywillams <70780931+saltyamogh@users.noreply.github.com>
2020-11-29 13:42:18 -0500

    finished question thread two

--- src/routes/forum/_posts.js
+++ src/routes/forum/_posts.js
@@ -16,17 +16,17 @@ const posts = [
 		html: `
 			<p>I really enjoy my calculus course, but I don’t know how finding a derivative would be useful in real life. Is there any jobs that use calculus concepts?</p>
 
-			<p>It's hard to find a job where math is actually done. I am in my last semester of my senior year I want to find some career options. Ideally I'd like to with calculus. I have been considering things in demand such as computational mathematics/physics, machine learning, operations research, etc. Does anyone know if there are many in demand opportunities for calculus?</p>
+			<p>It's hard to find a job where math is actually done. I am in my last semester of my senior year I want to find some career options. Ideally I'd like some of them to be with calculus. I have been considering things in demand such as computational mathematics/physics, machine learning, operations research, etc. Does anyone know if there are many in demand opportunities for calculus?</p>
 		`,
 		responses: [
 			{
-				author: 'henlo',
+				author: 'henlo [Professional Mentor]',
 				content: `
 					The PhD's I work with have have extensive mathematical backgrounds. None of them "do calculus" day to day. Instead, their job is to figure out the best way to get computers to "do" math for us. I'd be lost having prior background knowledge in calculus.
 				`
 			},
 			{
-				author: 'world',
+				author: 'world [Professional Mentor]',
 				content: `
 					Hi, in research, we use Calculus just about every day. There are some subtleties for smoothing out experimental noise and error checking, including things like making sure the area under the curve agrees with physical laws. PM me if you want to know more!
 				`
@@ -39,8 +39,23 @@ const posts = [
 		preview: 'I’m interested in pursuing a career in medicine, but I’m wondering how COVID-19might change job prospects and ...',
 		author: 'test_user_123',
 		html: `
-			<p>I’m interested in pursuing a career in medicine, but I’m wondering how COVID-19might change job prospects and ...</p>
-		`
+			<p>I’m interested in pursuing a career in medicine, but I’m wondering how COVID-19 might change this industry for future generations.</p> 
+			<p>Like will there be a significant impact for us youngsters in high school or will this pandemic be insignificant in the long run, please let me know!</p>
+		`,
+		responses: [
+			{
+				author: 'Riley Anderson [Professional Mentor]',
+				content: `
+					Hi, you have asked an excellent question and to be frank I don't exactly know. It really depends how determined people are willing to slow this pandemic down.
+				`
+			},
+			{
+				author: 'Benjamin Boardley [Professional Mentor]',
+				content: `
+					Hi, to answer your question I don't think it will affect you youngsters that badly. The impact will be minor as by the time you're in the workforce the effects of COVID-19 would've wore off
+				`
+			}
+		]
 	}
 ];
 
