71cfcf25901bc8e55e6271a56951765c1d61692f
Nico Bachner <yo@nicob.dev>
2020-11-30 11:33:32 +0100

    added 3rd post on forum

--- src/routes/forum/_posts.js
+++ src/routes/forum/_posts.js
@@ -9,7 +9,7 @@ const posts = [
 	{
 		title: 'What jobs actually use calculus?',
 		slug: 'calculus-jobs',
-		preview: 'I really enjoy my calculus course, but I don’t know how finding a derivative would be useful in real life. Is there ...',
+		preview: 'I really enjoy my calculus course, but I don’t know how finding a derivative would be useful in real life ...',
 		author: {
 			firstName: 'Robert',
 			lastName: 'Piccolo',
@@ -44,7 +44,30 @@ const posts = [
 		]
 	},
 	{
-		title: 'How is COVID-19 transforming the healthcare and pharmaceutical industries?',
+		title: 'What exactly does a software engineer do?',
+		slug: 'software-engineer',
+		preview: 'What do software engineers do on a day-to-day basis? Is it just coding all day or are there other important things ...',
+		author: {
+			firstName: 'Daisy',
+			lastName: 'Thompson',
+			type: 'High School Student'
+		},
+		html: `
+			<p>What do software engineers do on a day-to-day basis? Is it just coding all day or are there other important things that aren't coding related. I'm interested in software development as a career path, but I'm not sure if I'm willing to spend all my time just coding.</p>
+		`,
+		responses: [
+			{
+				author: {
+					firstName: 'Aditya',
+					lastName: 'Gupta',
+					type: 'Senior Software Engineer at Amazon'
+				},
+				content: "It really depends on what role you are assigned to. For example, I'm a senior software engineer, so I have more responsabilities than just writing code, such as supervising and teaching some of the new employees to make sure they know what they are doing. It's a very rewarding role because you get to see the joy on the faces on the people you teach."
+			}
+		]
+	},
+	{
+		title: 'Will COVID-19 change what we need to study?',
 		slug: 'covid-19-impact',
 		preview: 'I’m interested in pursuing a career in medicine, but I’m wondering how COVID-19 might change job prospects and ...',
 		author: {
@@ -54,7 +77,7 @@ const posts = [
 		},
 		html: `
 			<p>I’m interested in pursuing a career in medicine, but I’m wondering how COVID-19 might change this industry for future generations.</p> 
-			<p>Will there be a significant impact for us in high school or will this pandemic be insignificant in the long run.</p>
+			<p>Will there be a significant impact for us still in high school or will this pandemic be insignificant in the long run.</p>
 			<p>Please let me know!</p>
 		`,
 		responses: [
